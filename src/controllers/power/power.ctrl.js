b64947cca266ee7efdc945e32856ed9e317fc83e
Gois <matheusgoislimasilva@gmail.com>
2021-10-22 23:50:56 -0300

    upgrade code and example

--- src/controllers/power/power.ctrl.js
+++ src/controllers/power/power.ctrl.js
@@ -1,13 +1,28 @@
+const wol = require('wol');
+
 module.exports = {
   isConnected: (req, res, next) => {
     res.json({
       success: !!global.globalLGTV
     })
   },
+  async turnOn() {
+    console.log('Turning on...');
+    return new Promise((resolve, reject) => {
+      wol.wake('MAC ADDRESS', {
+        address: '192.168.18.3',
+        port: 3000
+      }, (error) => {
+        if (error) return reject(error);
+        console.log('Turned on');
+        resolve();
+      });
+    });
+  },
   connect: (req, res, next) => {
     try {
       var lgtv = require("../../lgtv")({
-        url: "ws://lgwebostv:3000",
+        url: "ws://192.168.100.68:3000",
       });
 
       lgtv.on("error", function (error) {
